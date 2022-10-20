d0a98957a623c4a13cf6dc733d7443c7dd7a2673
Betsue Weldemariam <btsegay0@gmail.com>
2022-10-20 16:32:12 +0300

    linked list under developement #1

---
+++ linked_list.rb
@@ -0,0 +1,23 @@
+class LinkedList
+
+  def initialize 
+
+  end
+
+end
+
+class Node 
+
+  def initialize
+
+  end
+
+  def value(value = nil)
+    
+  end
+
+  def next_node(next_node = nil)
+    
+  end
+
+end
